84d41cd1b3e96b24d22fa1f2348dd2bbbca9ccf9
forkanimahdi <forkanimahdi@gmail.com>
2026-03-02 15:07:42 +0000

    seeder

---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Product;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+
+
+        Product::insert([
+            [
+                "name" => "sbrdila",
+                "price" => 25
+            ],
+            [
+                "name" => "t-shirt",
+                "price" => 50
+            ],
+        ]);
+    }
+}
